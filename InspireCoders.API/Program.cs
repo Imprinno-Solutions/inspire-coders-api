f7bf53237f6ab6eec5a1ceead5e0572d421eb1ea
Michael Oyelami <michael.oyelami@frontdoorhome.com>
2022-07-25 14:27:43 +0100

    test endpoint

---
+++ InspireCoders.API/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var AllowInspireCodersClient = "_allowInspireCodersClient";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowInspireCodersClient,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:8080",
+                                              "http://localhost:4200");
+                      });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UsePathBase(new PathString("/api"));
+
+app.UseRouting();
+
+app.UseCors(AllowInspireCodersClient);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
